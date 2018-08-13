3bef4e918f928c82ba5ded37d73108a83e7344d9
Carlos P Navarrete Filho <karnav@gmail.com>
2018-08-13 12:01:54 -0300

    add Trash Can Button to Edit

--- src/scenes/Contacts/Edit/index.js
+++ src/scenes/Contacts/Edit/index.js
@@ -3,12 +3,20 @@ import { Text } from 'react-native'
 
 import NavigationHeader from '../../../components/Navigation/Header'
 import ContactsForm from '../ContactsForm'
+import ClearButton from '../../../components/ClearButton'
 import { Container, Content } from '../../../components/Utils'
 
 export default class EditContact extends Component {
-  static navigationOptions = ({navigation}) => ({
-    header: <NavigationHeader title='EDIT' onBack={() => navigation.goBack()} />
-  })
+  static navigationOptions = ({navigation}) => {
+    const { address } = navigation.getParam('contact', {})
+    return ({
+      header: <NavigationHeader title='EDIT' onBack={() => navigation.goBack()} rightButton={(
+        <ClearButton onPress={() => {
+          console.log(address)
+        }} />
+      )} />
+    })
+  }
 
   state = {
     contact: this.props.navigation.getParam('contact', {})
