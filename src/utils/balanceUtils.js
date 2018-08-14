d4d50d0e02b9c75d306452f9e2ad7eacbc5f08fc
Renan Nascimento <Renan@costanascimento.com.br>
2018-08-14 09:03:10 -0300

    added formatPrice and getPrice to balance utils

--- src/utils/balanceUtils.js
+++ src/utils/balanceUtils.js
@@ -1,3 +1,10 @@
+import Config from 'react-native-config'
+import axios from 'axios'
+
+import { formatNumber } from './numberUtils'
+
+const CRYPTO = ['BTC', 'ETH']
+
 export const orderBalances = (balances) => {
   let orderedBalances = []
   balances.forEach((balance) => {
@@ -11,4 +18,21 @@ export const orderBalances = (balances) => {
     ...orderedBalances.filter((balance) => !!balance),
     ...balances.filter((balance) => balance.name !== 'TRX' && balance.name !== 'TWX')
   ]
+}
+
+export const getPrice = async (currency) => {
+  try {
+    const { data: { data } } = await axios.get(`${Config.TRX_PRICE_API}/?convert=${currency}`)
+    return data.quotes[currency].price
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const formatPrice = (value, currency) => {
+  if (Number.isInteger(value) || CRYPTO.indexOf(currency) >= 0) {
+    return formatNumber(value)
+  } else {
+    return value.toFixed(2)
+  }
 }
